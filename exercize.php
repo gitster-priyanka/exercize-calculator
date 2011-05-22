ee24d4be5ed540a71d1ec15dedeca743e2f80374
priyanka menghani <menghani.priyanka@gmail.com>
2011-05-22 04:16:19 -0400

    first commit

---
+++ exercize.php
@@ -0,0 +1,52 @@
+<HTML>
+<HEAD>
+<STYLE TYPE="text/css">
+<!--
+BODY, P, TD
+{color: black; font-family: verdana; font-size: 10 pt}
+H1
+            {color: black; font-family: arial; font-size: 12 pt}
+            -->
+            </STYLE>
+            </HEAD>
+            <BODY>
+            <TABLE BORDER=0 CELLPADDING=10 WIDTH=100%>
+            <TR>
+            <TD BGCOLOR="#F0F8FF' ALIGN=CENTER VALIGN=TOP WIDTH=150>
+            </TD>
+            <TD BGCOLOR="#FFFFFF" ALIGN=LEFT VALIGN=TOP WIDTH=83%>
+            <H1>Workout calculator (radio buttons with arrays)</H1>
+            <P>Select one of the following exercises, and we'll tell
+                you how long <BR>
+                you'd have to do it to burn one pound of fat.</P>
+            <FORM METHOD="post" ACTION="wc_handler_ckbx.php">
+            <table>
+            <tr>
+              <td><input type="radio"
+                   name=”exercise” value=”0”>&nbsp;Biking/cycling</td>
+            </tr><tr>
+              <td><input type="radio"
+                   name="exercise" value="1">&nbsp;Running</td>
+            </tr><tr>
+              <td><input type="radio"
+                   name="exercise" value="2">&nbsp;Soccer/football</td>
+            </tr><tr>
+              <td><input type="radio"
+                   name="exercise" value="3">&nbsp;Stairclimber</td>
+            </tr><tr>
+              <td><input type="radio"
+                   name="exercise" value="4">&nbsp;Weightlifting</td>
+            </tr><tr>
+              <td>&nbsp;</td>
+            </tr><tr>
+              <td><input type="submit"
+                   name="submit" value="Burn, baby, burn!"></td>
+            </tr>
+            </table>
+            </FORM>
+            </TD>
+            </TR>
+            </TABLE>
+            </BODY>
+            </HTML>
+
